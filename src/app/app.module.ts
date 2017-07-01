0c519f4fb4f2641e3df7e16a440316c2554f5f35
Unknown <xano3425@gmail.com>
2017-07-01 14:03:25 -0700

    Add layout/header from current

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule

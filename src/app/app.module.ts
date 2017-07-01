1a468bc63bec2872f5c9d208999b8a1fcf313458
Unknown <xano3425@gmail.com>
2017-07-01 14:05:49 -0700

    Add layout/footer from current

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent
     AppComponent,
+    FooterComponent,
     HeaderComponent,
   ],
   imports: [

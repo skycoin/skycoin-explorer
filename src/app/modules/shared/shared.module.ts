9901bb991a729e71db763a396100c770f01eb67d
stdevDcmorales <dev0004@simelo.tech>
2018-01-05 20:15:01 -0500

    Add angular-font-awesome

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,19 +3,22 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { LoaderComponent } from "app/modules/shared/components/loader/loader.component";
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         HttpModule,
+        AngularFontAwesomeModule
     ],
     declarations: [LoaderComponent],
     exports: [
         CommonModule,
         FormsModule,
         HttpModule,
-        LoaderComponent
+        LoaderComponent,
+        AngularFontAwesomeModule
     ]
 })
 export class SharedModule { }  

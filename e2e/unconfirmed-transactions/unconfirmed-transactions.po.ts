cbc645f7fa45bd1f20bdd3a12b0580b524d2fbbe
Senyoret1 <34079003+Senyoret1@users.noreply.github.com>
2018-09-10 15:39:52 -0400

    Updated e2e tests

--- e2e/unconfirmed-transactions/unconfirmed-transactions.po.ts
+++ e2e/unconfirmed-transactions/unconfirmed-transactions.po.ts
@@ -1,5 +1,23 @@
 import { browser, by, element } from 'protractor';
 
 export class UnconfirmedTransactionsPage {
-  
+
+  getNumberOfTransactions() {
+    return element(by.css('.element-details > div:nth-of-type(1) > div'))
+      .getText()
+      .then(text => Number(text));
+  }
+
+  getSize() {
+    return element(by.css('.element-details > div:nth-of-type(2) > div'))
+      .getText()
+      .then(text => Number(text.split(' ')[0]));
+  }
+
+  getTimestampValidity(index: number) {
+    return element(by.css('.element-details > div:nth-of-type(' + index + ') > div'))
+      .getText()
+      .then(text => !isNaN((new Date(text)).getTime()));
+  }
+
 }

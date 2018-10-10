b136691259e491ff0c20a3d05c8245a240e252b0
Senyoret1 <34079003+Senyoret1@users.noreply.github.com>
2018-10-10 17:20:42 -0400

    Update /api/health

--- explorer.go
+++ explorer.go
@@ -1125,25 +1125,32 @@ var apiEndpoints = []APIEndpoint{
 		ExampleResponse: `{
       "blockchain": {
           "head": {
-              "seq": 21175,
-              "block_hash": "8a3e0aac619551ae009cfb28c2b36bb1300925f74da770d1512072314f6a4c80",
-              "previous_block_hash": "001eb7911b6a6ab7c75feb88726dd2bc8b87133aebc82201c4404537eb74f7ac",
-              "timestamp": 1523168686,
-              "fee": 2,
+              "seq": 58894,
+              "block_hash": "3961bea8c4ab45d658ae42effd4caf36b81709dc52a5708fdd4c8eb1b199a1f6",
+              "previous_block_hash": "8eca94e7597b87c8587286b66a6b409f6b4bf288a381a56d7fde3594e319c38a",
+              "timestamp": 1537581604,
+              "fee": 485194,
               "version": 0,
-              "tx_body_hash": "36be8d70d1e9f70b340ea7ecf0b247c27086bad10568044c1196fe150f6cea1b"
+              "tx_body_hash": "c03c0dd28841d5aa87ce4e692ec8adde923799146ec5504e17ac0c95036362dd",
+              "ux_hash": "f7d30ecb49f132283862ad58f691e8747894c9fc241cb3a864fc15bd3e2c83d3"
           },
-          "unspents": 14750,
-          "unconfirmed": 0,
-          "time_since_last_block": "12m6s"
+          "unspents": 38171,
+          "unconfirmed": 1,
+          "time_since_last_block": "4m46s"
       },
       "version": {
-          "version": "0.23.0",
-          "commit": "f61b4319c2f146a5ad86f7cbda26a1ba6a09998d",
+          "version": "0.24.1",
+          "commit": "8798b5ee43c7ce43b9b75d57a1a6cd2c1295cd1e",
           "branch": "develop"
       },
-      "open_connections": 30,
-      "uptime": "13.686460853s"
+      "open_connections": 8,
+      "uptime": "6m30.629057248s",
+      "csrf_enabled": true,
+      "csp_enabled": true,
+      "wallet_api_enabled": true,
+      "gui_enabled": true,
+      "unversioned_api_enabled": false,
+      "json_rpc_enabled": false
 }`,
 	},
 }

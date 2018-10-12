1631ef6c704dc9ec46e1e2f5c791e55127719bff
Senyoret1 <34079003+Senyoret1@users.noreply.github.com>
2018-10-12 12:20:03 -0400

    Limit how many inputs/outputs to show

--- src/app/components/layout/transaction-info/transaction-info.component.ts
+++ src/app/components/layout/transaction-info/transaction-info.component.ts
@@ -7,8 +7,47 @@ import { HeaderConfig } from 'app/app.config';
   styleUrls: ['./transaction-info.component.scss']
 })
 export class TransactionInfoComponent {
-  @Input() transaction: any;
+  private maxElements = 10;
+  private transactionInternal: any;
+
+  totalInputs = 0;
+  nextInputsGroup = 0;
+  inputsToShow: any[] = [];
+  totalOutputs = 0;
+  nextOutputsGroup = 0;
+  outputsToShow: any[] = [];
+
+  @Input()
+  set transaction(value: any) {
+    this.transactionInternal = value;
+
+    this.totalInputs = this.transaction.inputs.length;
+    this.totalOutputs = this.transaction.outputs.length;
+
+    this.showMoreInputs();
+    this.showMoreOutputs();
+  }
+  get transaction(): any {
+    return this.transactionInternal;
+  }
+
   @Input() unconfirmed = false;
   
   constructor() { }
+
+  showMoreInputs() {
+    const currentNumber = this.inputsToShow.length;
+    for (let i=currentNumber; i<Math.min(currentNumber + this.maxElements, this.totalInputs); i++) {
+      this.inputsToShow.push(this.transaction.inputs[i]);
+    }
+    this.nextInputsGroup = Math.min(this.totalInputs - this.inputsToShow.length, this.maxElements);
+  }
+
+  showMoreOutputs() {
+    const currentNumber = this.outputsToShow.length;
+    for (let i=currentNumber; i<Math.min(currentNumber + this.maxElements, this.totalOutputs); i++) {
+      this.outputsToShow.push(this.transaction.outputs[i]);
+    }
+    this.nextOutputsGroup = Math.min(this.totalOutputs - this.outputsToShow.length, this.maxElements);
+  }
 }
